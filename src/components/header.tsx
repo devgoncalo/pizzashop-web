954e0bf1a6400d4ff3bf5e9e949807f3b2113efc
devgoncalo <goncalopintoneto@gmail.com>
2024-02-22 22:29:55 +0000

    feat: ThemeToggle and Account Menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,14 @@
 import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
 
+import { AccountMenu } from './account-menu'
 import { NavLink } from './nav-link'
+import { ThemeToggle } from './theme/theme-toggle'
 import { Separator } from './ui/separator'
 
 export function Header() {
   return (
     <div className="border-b">
-      <div className="flex h-16 gap-6 px-6">
+      <div className="flex h-16 items-center gap-6 px-6">
         <Pizza className="size-6" />
 
         <Separator orientation="vertical" className="h-6" />
@@ -21,6 +23,11 @@ export function Header() {
             Pedidos
           </NavLink>
         </nav>
+
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+          <AccountMenu />
+        </div>
       </div>
     </div>
   )
